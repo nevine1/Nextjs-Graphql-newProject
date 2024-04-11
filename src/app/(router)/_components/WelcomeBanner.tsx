212ca736cb943d7e0308b751ab782b8309f44ae5
nevine1 <venasamaan@gmail.com>
2024-04-10 23:02:38 -0400

    changes 2 on Apr 10

--- src/app/(router)/_components/WelcomeBanner.tsx
+++ src/app/(router)/_components/WelcomeBanner.tsx
@@ -6,8 +6,12 @@ interface Props {
 
 export const WelcomeBanner = (props: Props) => {
     return (
-        <div>
-           <h1 className="text-[40px]">Hello Banner</h1>
+        <div className="p-3">
+           <h1 className="font-bold text-[40px]">Welcome to 
+                <span className="text-primary"> Vena</span> academy
+           </h1>
+           <h1 className="sm:text-[16px] text-[18px] text-grey-600">Learn, explore and build web applications</h1>
+          
         </div>
     )
 }
--- src/app/(router)/courses/page.tsx
+++ src/app/(router)/courses/page.tsx
@@ -2,7 +2,9 @@ import { WelcomeBanner } from "../_components/WelcomeBanner"
 export default function page () {
     return(
         <div className=" grid grid-cols-1 md:grid-cols-3 gap-3">
-            <div className="col-span-2">Banner</div>
+            <div className="col-span-2">
+                <WelcomeBanner/>
+            </div>
             <div>Right Section </div>
             
 
