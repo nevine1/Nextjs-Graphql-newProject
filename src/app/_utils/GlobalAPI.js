5f41c3602550f8d371924f93694479f98c8198e2
nevine1 <venasamaan@gmail.com>
2024-04-12 22:26:48 -0400

    graphql api code

--- src/app/_utils/GlobalAPI.js
+++ src/app/_utils/GlobalAPI.js
@@ -1,1 +1,36 @@
+import { gql } from "graphql-request"
+
 const MASTER_URL="https://api-ca-central-1.hygraph.com/v2/"+process.env.NEXT_PUBLIC_HYGRAPH_API_KEY+"/master"
+
+const getAllCourseList = async () =>{
+    const query = gql`
+        query CourseLists {
+            courseLists {
+                author
+                createdAt
+                description
+                free
+                id
+                name
+                totalParts
+                uploadedDate
+                youtbueUrl
+                bannerPicture {
+                url
+                }
+                updatedBy {
+                id
+                name
+                }
+                createdBy
+            }
+        }
+    `;
+    
+    const result = await request(MASTER_URL, query);
+    return result; 
+}
+
+export default {
+    getAllCourseList
+}
