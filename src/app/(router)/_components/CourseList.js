b4f4b8d298fccb08664aef882fa25bac40f15df0
nevine1 <venasamaan@gmail.com>
2024-04-19 22:46:14 -0400

    fetching api data

--- src/app/(router)/_components/CourseList.js
+++ src/app/(router)/_components/CourseList.js
@@ -1,17 +1,26 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import  GlobalAPI  from '../../_utils/GlobalAPI'
 function CourseList() {
-  
+  const [courses, setCourses] = useState([])
 useEffect(()=> {
   fetchCourses();
 }, [])
   const fetchCourses = () => {
     GlobalAPI.getAllCourseList().then(resp=>{
       console.log(resp)
+      setCourses(resp?.courseLists)
     })
   }
   return (
-    <div>CourseList</div>
+    <div>
+      {
+        courses.map((course) =>{
+          <div>
+            { course.name}
+          </div>
+        })
+      }
+    </div>
   )
 }
 
